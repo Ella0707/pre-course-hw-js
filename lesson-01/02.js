6549f755dfb9340b40263562ba631d457466430b
Ella0707 <pakelvi77@gmail.com>
2023-07-26 17:55:10 +0300

    add random numder

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,3 +1,8 @@
 // code
 let number1 = Math.floor(Math.random() * 100);
-console.log(number1%2 ? number1 + ':Это нечетное число' : number1 + ':Это четное число' )
+
+if (number1 % 2 === 0) {
+  console.log(number1 + ':Это четное число')
+} else {
+  console.log(number1 + ':Это нечетное число')
+}
