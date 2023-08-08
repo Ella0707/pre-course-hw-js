9fe6f119ba24db4ff9ce1fec79569918f801b882
Ella0707 <pakelvi77@gmail.com>
2023-08-08 14:41:17 +0300

    task 02/07

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -10,8 +10,10 @@ let passportMarried = {
 let passportMarried2 = {
   ...passportMarried,
   married: true,
+  address: {...passportMarried},
 };
 
+passportMarried2.married = true;
 
 console.log(passportMarried);
 console.log(passportMarried2);
