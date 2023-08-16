7f8e9e70dec0cc445bb2e200a21c9b5cb0ff65b1
Ella0707 <pakelvi77@gmail.com>
2023-08-16 16:27:41 +0300

    task 03/07

---
+++ lesson-03/07.js
@@ -0,0 +1,14 @@
+const numbers = [
+  [1, 2, 3, 4, 5],
+  [11, 20, 33, 40, 55],
+  [111, 200, 333, 400, 555],
+]
+
+
+for (i = 0; i < numbers.length; i++) {
+  for (k=0; k < numbers[i].length; k++) {
+    if(numbers[i][k] % 2 == 0) {
+      console.log(numbers[i][k])
+    }
+  }
+}
