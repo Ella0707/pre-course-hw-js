5256f278e332ea3b4b66cf44fc43d79ba278be0e
Ella0707 <pakelvi77@gmail.com>
2023-08-17 08:05:40 +0300

    task 03/07

--- lesson-03/07.js
+++ lesson-03/07.js
@@ -2,13 +2,8 @@ const numbers = [
   [1, 2, 3, 4, 5],
   [11, 20, 33, 40, 55],
   [111, 200, 333, 400, 555],
-]
+];
 
+const result = numbers.map(subArr => subArr.filter(num => num % 2 === 0));
 
-for (i = 0; i < numbers.length; i++) {
-  for (k=0; k < numbers[i].length; k++) {
-    if(numbers[i][k] % 2 == 0) {
-      console.log(numbers[i][k])
-    }
-  }
-}
+console.log(result);
